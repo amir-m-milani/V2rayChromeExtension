37c3a77216004ce1fa9caa81df1e99a20ff4716b
amir-m-milani <amirmilani08@gmail.com>
2024-07-29 10:41:58 +0330

    make the structure of simple chrome extension

---
+++ js/options.js
@@ -0,0 +1,5 @@
+document.getElementById('settingsForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Add your save logic here
+    alert('Settings saved.');
+});
---
+++ js/popup.js
@@ -0,0 +1,8 @@
+document.getElementById('connectBtn').addEventListener('click', function () {
+    // Add your connect logic here
+    alert('Connecting...');
+});
+document.getElementById('disconnectBtn').addEventListener('click', function () {
+    // Add your disconnect logic here
+    alert('Disconnecting...');
+});
